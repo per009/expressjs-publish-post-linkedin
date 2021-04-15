c8d202705093dff93224f1106fe4a7cf69863419
Percy Vier <pvier92@gmail.com>
2021-04-14 21:24:18 -0500

    Testes some features and API

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const path = require('path');
 const express = require('express');
@@ -6,11 +6,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
+const session = require('express-session');
 const validator = require('validator');
 const API = require('./lib/API');
 const { sessionName, sessionKeys } = require('./config');
 
-
+var mySession = {session:{}}
 app.disable('x-powered-by');
 
 app.set('view engine', 'ejs');
@@ -22,14 +23,29 @@ app.use(cookieSession({
     keys: sessionKeys
 }));
 
+
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}))
+
 app.get('/', async (req, res) => {
-    const isAuthorized = (req.session.authorized);
+    console.log("STARTING")
+    console.log("Session: ",mySession.session.authorized)
+    const isAuthorized = (mySession.session.authorized);
     if(!isAuthorized) {
+        console.log("NOT AUTHORIZED")
         res.render('index', { isAuthorized, id: '' });
-    } else {
+    } else { 
+        console.log("AUTHORIZED");
         try {
-            const id = await API.getLinkedinId(req);
+            const id = await API.getLinkedinId(req,mySession.session.token);
+            console.log("TOKEN", mySession.session.token);
             res.render('index', { isAuthorized, id });
+            const token = mySession.session.token;
+            const askUpload = await API.askUploadImage(req, id,token);
         } catch(err) {
             res.send(err);
         }
@@ -37,21 +53,36 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/auth', (req, res) => {
+    console.log("Authorizing")
     res.redirect(API.getAuthorizationUrl());
 });
 
 app.get('/callback', async (req, res) => {
+    console.log("RECEIVE CALLBACK")
+    console.log("Code: ",req.query.code)
     if(!req.query.code) {
         res.redirect('/');
         return;
     }
     try {
+        console.log("GOT CODE")
         const data = await API.getAccessToken(req);
+        console.log("TOKEN: ", data.access_token)
         if(data.access_token) {
             req.session.token = data.access_token;
             req.session.authorized = true;
+            mySession.session.token = data.access_token;
+            mySession.session.authorized = true;
+            res.redirect("/");
+            // req.session.save(function(err) {
+            //     if(!err) {
+            //         //Data get lost here
+            //         res.redirect("/");
+            //     }
+            //  });
         }
-        res.redirect('/');
+        
+        
     } catch(err) {
         res.json(err);
     }
@@ -61,33 +92,38 @@ app.post('/publish', async (req, res) => {
     const { title, text, url, thumb, id } = req.body;
     const errors = [];
 
-    if(validator.isEmpty(title)) {
-        errors.push({ param: 'title', msg: 'Invalid value.'});
-    }
-    if(validator.isEmpty(text)) {
-        errors.push({ param: 'text', msg: 'Invalid value.'});
-    }
-    if(!validator.isURL(url)) {
-        errors.push({ param: 'url', msg: 'Invalid value.'});
-    }
-    if(!validator.isURL(thumb)) {
-        errors.push({ param: 'thumb', msg: 'Invalid value.'});
-    }
+    // if(validator.isEmpty(title)) {
+    //     errors.push({ param: 'title', msg: 'Invalid value.'});
+    // }
+    // if(validator.isEmpty(text)) {
+    //     errors.push({ param: 'text', msg: 'Invalid value.'});
+    // }
+    // if(!validator.isURL(url)) {
+    //     errors.push({ param: 'url', msg: 'Invalid value.'});
+    // }
+    // if(!validator.isURL(thumb)) {
+    //     errors.push({ param: 'thumb', msg: 'Invalid value.'});
+    // }
 
     if(errors.length > 0) {
         res.json({ errors });
     } else {
         const content = {
-            title: title,
-            text: text,
-            shareUrl: url,
-            shareThumbnailUrl: thumb
+            title: title || "",
+            text: text || "",
+            shareUrl: url || "",
+            shareThumbnailUrl: thumb || ""
         };
 
         try {
-            const response = await API.publishContent(req, id, content);
+            console.log("TOKEN:", mySession.session.token)
+            const token = mySession.session.token;
+            //const askUpload = await API.askUploadImage(req, id, content,token);
+            //const upload = await API.uploadImage(req, id, content,token);
+            const response = await API.publishContent(req, id, content,token);
             res.json({ success: 'Post published successfully.' });
         } catch(err) {
+            console.log(err);
             res.json({ error: 'Unable to publish your post.' });
         }
     }
